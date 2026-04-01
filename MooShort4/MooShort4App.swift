a7317854dc2b4cbb6cdc27a621a6bc2ac34fe1a9
samtari1 <samtariyang@gmail.com>
2026-04-01 02:14:03 -0400

    Initial Commit

---
+++ MooShort4/MooShort4App.swift
@@ -0,0 +1,17 @@
+//
+//  MooShort4App.swift
+//  MooShort4
+//
+//  Created by Quanpeng Yang on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MooShort4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MooShort4Tests/MooShort4Tests.swift
@@ -0,0 +1,19 @@
+//
+//  MooShort4Tests.swift
+//  MooShort4Tests
+//
+//  Created by Quanpeng Yang on 4/1/26.
+//
+
+import Testing
+@testable import MooShort4
+
+struct MooShort4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
